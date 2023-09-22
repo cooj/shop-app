ebb3a83ac96e6aa82f637114acb9c19ffd316b3b
xiaoqian1oo <2362420217@qq.com>
2023-09-22 17:03:33 +0800

    轮播图更改

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,44 +1,36 @@
 <template>
     <view>
         <view class="w100% h100rpx">
-            搜索框
+            <!-- 搜索框 -->
         </view>
-        <view class="uni-margin-wrap">
-            <swiper class="swiper" circular indicator-dots="true" autoplay="true" interval="5000" duration="500">
-                <!--   <swiper-item>
-                    <view v-for="(item, index) in defData.swiperList" :key="index" class="swiper-item uni-bg-red">
-                        <image
-                            :src="item.url" mode="aspectFill" />
-                    </view>
-                </swiper-item> -->
-                <swiper-item>
-                    <view class="swiper-item uni-bg-red">
-                        <image
-                            src="https://goyojo.oss-cn-shenzhen.aliyuncs.com/20230522/202305221547174353.jpg?x-oss-process=image/quality,Q_50" />
-                    </view>
-                </swiper-item>
-                <swiper-item>
-                    <view class="swiper-item uni-bg-red">
-                        <image
-                            src="https://goyojo.oss-cn-shenzhen.aliyuncs.com/20230530/202305301913593711.jpg?x-oss-process=image/quality,Q_50" />
+        <view class="">
+            <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+                <swiper-item v-for="(item, i) in defData.swiperList" :key="i">
+                    <view class="swiper-item">
+                        <image :src="item.url" class="swiper-img" />
                     </view>
                 </swiper-item>
             </swiper>
         </view>
         <view class="w100% h200rpx">
-            自营品牌
+            <!-- 自营品牌 -->
         </view>
         <view class="w100% h100rpx">
-            自定义分类
+            <!-- 自定义分类 -->
         </view>
     </view>
 </template>
 
 <script setup lang="ts">
-// import { BannerApi } from '@/service/api/home/banner'
-
 const defData = reactive({
-    swiperList: [],
+    swiperList: [
+        {
+            url: 'https://goyojo.oss-cn-shenzhen.aliyuncs.com/20230607/202306070843255789.jpg?x-oss-process=image/quality,Q_50',
+        },
+        {
+            url: 'https://goyojo.oss-cn-shenzhen.aliyuncs.com/20230522/202305221547174353.jpg?x-oss-process=image/quality,Q_50',
+        },
+    ],
 })
 
 const initData = async () => {
@@ -47,13 +39,9 @@ const initData = async () => {
 </script>
 
 <style>
-.uni-margin-wrap {
-    width: 690rpx;
-    width: 100%;
-}
-
-.swiper {
+swiper {
     height: 300rpx;
+    margin:0 10px;
 }
 
 .swiper-item {
@@ -62,24 +50,9 @@ const initData = async () => {
     line-height: 300rpx;
     text-align: center;
 }
-
-.swiper-list {
-    margin-top: 40rpx;
-    margin-bottom: 0;
-}
-
-.uni-common-mt {
-    margin-top: 60rpx;
-    position: relative;
-}
-
-.info {
-    position: absolute;
-    right: 20rpx;
-}
-
-.uni-padding-wrap {
-    width: 550rpx;
-    padding: 0 100rpx;
+.swiper-img{
+    width: 100%;
+    height: 100%;
+    border-radius: 20px;
 }
 </style>
