b098c7d443fbd0872b85b46747323c2693778602
cooj <z8888788@163.com>
2023-09-25 14:12:38 +0800

    chore(other): 文件代码格式更改，typings文件夹更改为types，只用于类型的存放

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -8,10 +8,10 @@ type ServiceEnvType = 'dev' | 'test' | 'prod'
 
 /** 后台服务的环境配置 */
 interface ServiceEnvConfig {
-  url: string
-  [key: string]: any
+    url: string
+    [key: string]: any
 }
 
 interface ImportMetaEnv {
-  readonly VITE_SERVICE_ENV?: ServiceEnvType;
+    readonly VITE_SERVICE_ENV?: ServiceEnvType;
 }
