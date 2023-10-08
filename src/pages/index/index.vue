7bf36ed5b023f44a45f0624ab636909919d53a69
cooj <z8888788@163.com>
2023-10-08 10:30:28 +0800

    feat(other): 设置首页显示所有路由

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,15 @@
 <template>
     <view class="bg-#e2f3fe p-4">
+        <uni-list>
+            <uni-list-item v-for="item in routeList" :key="item.path" link :to="item.path" :show-arrow="false">
+                <template #body>
+                    <text class="font-bold">
+                        {{ item.meta.navigationBar.titleText }}
+                    </text>:{{ item.path }}
+                </template>
+            </uni-list-item>
+        </uni-list>
+
         <view class="text-lg mb-2 text-c2">
             使用 class
         </view>
@@ -51,4 +61,24 @@ const flag = ref(true)
 function switchFlag() {
     flag.value = !flag.value
 }
+
+const routeList = ref<any[]>([])
+
+// const router = useRouter()
+// console.log('router :>> ', router)
+const routes = getCurrentPages() // 获取当前打开过的页面路由数组
+// console.log('routes :>> ', routes)
+// console.log(routes.$getAppWebview())
+
+onShow((option) => {
+    const pro = routes[0].$.appContext.provides
+
+    const tt = Object.getOwnPropertySymbols(pro)
+    // console.log('pro[x] :>> ', pro[tt[1]])
+
+    const routeData = pro[tt[1]]
+
+    routeList.value = routeData.getRoutes()
+    console.log('routeList.value :>> ', routeList.value)
+})
 </script>
